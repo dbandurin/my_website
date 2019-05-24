14356833b4eb2b9be392d7939e8bb740745c50fb
Dimitri Bandurin <dbandurin@strikesocial.com>
2019-05-23 22:57:35 -0500

    first commit

--- myapp.py
+++ myapp.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5001, debug=True)

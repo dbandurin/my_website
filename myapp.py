14bff82a3e1f79fc9cd247d7d74de5d71c3aa04e
Dmitry Bandurin <dmitry.bandurin@Dmitrys-MacBook-Pro-2.local>
2020-03-28 15:20:52 -0500

    New framework created

--- myapp.py
+++ myapp.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
+
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
+def index():
+    print('Index called')
+    return render_template('home_product.html')
 
+@app.route("/blog.html", methods=['GET', 'POST'])
+def blog():
+    print('Solutions called, request.method = ',request.method)
+    # if request.method == 'POST':
+    #     return redirect(url_for('index'))
+    return render_template('blog.html')
+
+@app.route("/test.html", methods=['GET', 'POST'])
+def test_dashboard():
+    print('Solutions called, request.method = ',request.method)
+    # if request.method == 'POST':
+    #     return redirect(url_for('index'))
+    
+    # #else:
+    # #Dashboard
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
